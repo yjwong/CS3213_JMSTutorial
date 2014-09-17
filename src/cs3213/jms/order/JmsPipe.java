f70fed5c3de77434ffd02c97dc3f6720846f4d26
Wong Yong Jie <yjwong92@gmail.com>
2014-09-17 15:18:55 +0800

    Implemented the JMS-based pipe.

--- src/cs3213/jms/order/JmsPipe.java
+++ src/cs3213/jms/order/JmsPipe.java
@@ -1,5 +1,11 @@
 package cs3213.jms.order;
 
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Properties;
+
 /**
  * Matric 1: A0098862L
  * Name   1: Ang Hui Loon
@@ -12,21 +18,65 @@ package cs3213.jms.order;
 
 public class JmsPipe implements IPipe {
 
-    public JmsPipe(String simpleConnectionFactory, String simpleQueue) {
+    private QueueConnectionFactory qconFactory;
+    private QueueConnection qcon;
+    private QueueSession qsession;
+    private QueueSender qsender;
+    private QueueReceiver qreceiver;
+    private Queue queue;
+    private TextMessage msg;
+
+    public JmsPipe(String simpleConnectionFactory, String simpleQueue)
+            throws NamingException, JMSException {
+        Context context = getInitialContext();
+        qconFactory = (QueueConnectionFactory) context.lookup(simpleConnectionFactory);
+        qcon = qconFactory.createQueueConnection();
+        qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+        queue = (Queue) context.lookup(simpleQueue);
+        qsender = qsession.createSender(queue);
+        qreceiver = qsession.createReceiver(queue);
+        msg = qsession.createTextMessage();
+        qcon.start();
     }
 
     @Override
-    public void write(Order s) {
-
+    public void write(Order order) {
+        try {
+            msg.setText(order.toString());
+            qsender.send(msg);
+        } catch (JMSException e) {
+            System.err.println("[JmsPipe] Failed to write order.");
+        }
     }
 
     @Override
     public Order read() {
+        try {
+            TextMessage msg = (TextMessage) qreceiver.receive();
+            return Order.fromString(msg.getText());
+        } catch (JMSException e) {
+            System.err.println("[JmsPipe] Failed to receive order.");
+        }
+
         return null;
     }
 
     @Override
     public void close() {
+        try {
+            qreceiver.close();
+            qsender.close();
+            qcon.close();
+        } catch (JMSException e) {
+            System.err.println("[JmsPipe] Failed to close pipe.");
+        }
+    }
 
+    private InitialContext getInitialContext() throws NamingException {
+        Properties props = new Properties();
+        props.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+        props.put(Context.PROVIDER_URL, "jnp://localhost:1099");
+        props.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
+        return new InitialContext(props);
     }
 }
